a791e2746e4f654ef5ae4413eb69b790f58b3f56
AlexJoseSebastian <124261020+AlexJoseSebastian@users.noreply.github.com>
2024-08-12 12:56:18 +0000

    variables(const,let,var)

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const account_id=1234;
+let account_mail="asldfja;sl@gmail.accountmail"
+let account_password="1234"
+account_city="jaipur" //not preffered 
+let account_state;
+
+/*{
+    inside curley bracket is scope
+}*/
+
+/* if (condition) {
+    this is function scope
+}*/
+
+/*
+don't use var bez of it cuz issue in block scope and funtional scope
+*/
+
+console.log(account_id)
+console.table([account_id, account_mail, account_password, account_city, account_state])
