4b0033cc8e26be75c50e1f051b718b339b938971
AlexJoseSebastian <124261020+AlexJoseSebastian@users.noreply.github.com>
2024-08-12 17:33:11 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score = "alex"
+
+// let valueInNumber = Number(score) 
+ // NaN - not as number;
+// console.log(valueInNumber)
+
+// let scores=true
+let scores="33abc"
+
+let value = Number(scores)
+console.log(value)
+
+// "33" => 33 
+// "33abc" => NaN(not as number) but its type is number
+
+let isLogginedIn = true
+
+let valInBool = Boolean(isLogginedIn)
+console.log(valInBool) 
+
+// if we leave it as empty string it will give false as value
+let islog = ""
+// let valB = Boolean(islog)
+// console.log(valB)
+
+// if we write something it will give true as value
+let isL="Alex"
+// let VB = Boolean(isL)
+// console.log(VB)
+
+// 1 => true, 0 => false
+// "" => false
+// "alex" => true
+
+
+let somenumber = 44
+let isString = String(somenumber)
+console.log(isString);
+console.log(typeof isString)
+
+
+
