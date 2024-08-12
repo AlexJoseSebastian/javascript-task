d9b198ffd8d84bbd2e65c19b66246d8e24866b55
AlexJoseSebastian <124261020+AlexJoseSebastian@users.noreply.github.com>
2024-08-12 13:18:41 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // tread all js code as new version
+
+// alert( 3+3) // will not run because we are not using browser
+
+let name="alex" // string
+let age = 21 // integer
+let login = false // boolean
+
+/* datatypes (primitive)
+
+// number => 2 to power 53
+// bigint
+// string
+// null => standalone value (type is an object)
+// boolean => true/false
+// undefined  type is undefined.
+// symbol => uunique
+
+// object
+
+*/
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
