0379426df476f9c0e87af0ea4d26a96082f90049
AlexJoseSebastian <124261020+AlexJoseSebastian@users.noreply.github.com>
2024-08-16 06:13:26 +0000

    memories stack(primitive) and Heap(Non-primitive)

--- 01_Basics/datatype-summary.js
+++ 01_Basics/datatype-summary.js
@@ -31,3 +31,27 @@ const myfunction  = function(){
 console.log(typeof myfunction); // funtion data type is object function(rest other are same array,object they are called functions)
 console.log(typeof Bigbignumber);
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (primitive datatype) , Heap(Non-primitive datatype)
+
+let myYoutubename = "Alexdotcom";
+
+let anothername = myYoutubename;
+anothername = "chai with code"
+
+console.log(myYoutubename) 
+console.log(anothername) // stact will not change the original value but will change the copied value
+
+// objects
+let User1 =  {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let User2=User1
+
+User2.email="alex@gmail.com"
+
+console.log(User1.email);
+console.log(User2.email);  // In Heap you will get the `referrence of original data` So. the `changes which are made are in the original data`
